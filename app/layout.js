612a0f651f5415755f088676979ede9cd94105ab
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-08-30 09:30:49 +0800

    add bebas font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Bebas_Neue, Roboto_Slab } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const bebas = Bebas_Neue({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bebas.className}>{children}</body>
     </html>
   )
 }
