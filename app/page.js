2635e291cf2efded158621a6c96059b6735fc264
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-06 13:13:22 +0800

    add navbar component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Billboard from '@/components/Billboard'
+import Navbar from '@/components/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Billboard title="test" description="testing" imageURL="/images/Kingdom.png" />
+    <>
+      <Navbar />
+      <Billboard title="test" description="testing" imageURL="/images/Kingdom.png" />
+    </>
   )
 }
