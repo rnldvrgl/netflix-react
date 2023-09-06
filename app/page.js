1f41c248bda737d4bff92d37d85416bbde3b0421
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-06 17:03:11 +0800

    update

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
+    <div className=''>
       <Navbar username="Test" />
       <Billboard title="test" description="testing" imageURL="/images/Kingdom.png" />
-    </>
+    </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,22 +3,30 @@ import Link from "next/link";
 const Navbar = (props) => {
     const { username } = props;
     return (
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>My List</li>
-            </ul>
-
-            <nav>
-                <div>
-                    <button>
-                        <p>{username}</p>
-                    </button>
-                    <div>
-                        <Link href={'#'}>Sign Out</Link>
+        <div className="fixed top-0 z-50 w-full text-white10 bg-gradient-to-b from-black via-transparent to-transparent">
+            <div className="flex p-5 px-4">
+                <Link href={'/'} className="flex items-center mb-4 text-base font-medium text-white10">
+                    <div className="w-32 text-red">
+                        Netflix
                     </div>
-                </div>
-            </nav>
+                </Link>
+
+                <ul className="flex w-1/2 ml-6 text-base list-none">
+                    <li className="mr-3 text-base font-semibold cursor-pointer">Home</li>
+                    <li className="cursor-pointer">My List</li>
+                </ul>
+
+                <nav className="flex ml-auto">
+                    <div>
+                        <button className="flex items-center overflow-hidden text-white">
+                            <p className="text-base">{username}</p>
+                        </button>
+                        <div className="absolute pt-2 pr-2 mt-2 ml-auto border shadow-sm bg-black50 border-r-1 border-blue">
+                            <Link href={'#'}>Sign Out</Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
     );
 }
