93712beebb0f2ebc846341ab252708d1c5372d8a
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-12 07:59:27 +0800

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,10 +2,19 @@ import Image from "next/image";
 
 const Card = (props) => {
     const { imgUrl, size } = props;
+
+    const classMap = {
+        large: "relative w-300 h-170",
+        medium: "relative w-158 h-280",
+        small: "relative w-218 h-434",
+    };
+    console.log(imgUrl)
+
     return (
-        <div>
-            Card
-            <Image src={imgUrl} alt="image" width="300" height="300" />
+        <div className="mr-1 cursor-pointer">
+            <div className={classMap[size]}>
+                <Image src={imgUrl} alt="image" fill className="top-0 bottom-0 left-0 right-0 block object-cover object-center max-w-full rounded-md" />
+            </div>
         </div>
     );
 }
