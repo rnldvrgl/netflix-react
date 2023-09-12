cf1f79b9f3f2af22df9ff303675c611612d494ff
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-12 09:44:23 +0800

    update card

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,19 +4,23 @@ const Card = (props) => {
     const { imgUrl, size } = props;
 
     const classMap = {
-        large: "relative w-300 h-170",
-        medium: "relative w-158 h-280",
-        small: "relative w-218 h-434",
+        large: "relative min-w-[300px] w-[300px] min-h-[170px] h-[170px]",
+        medium: "relative w-[158px] min-w-[158px] h-[280px] min-h-[280px]",
+        small: "relative w-[218px] min-w-[218px] h-[434px] min-h-[434px]",
     };
-    console.log(imgUrl)
 
     return (
         <div className="mr-1 cursor-pointer">
             <div className={classMap[size]}>
-                <Image src={imgUrl} alt="image" fill className="top-0 bottom-0 left-0 right-0 block object-cover object-center max-w-full rounded-md" />
+                <Image
+                    src={imgUrl}
+                    alt="image"
+                    layout="fill"
+                    className="top-0 bottom-0 left-0 right-0 block object-cover object-center max-w-full rounded-md"
+                />
             </div>
         </div>
     );
-}
+};
 
 export default Card;
