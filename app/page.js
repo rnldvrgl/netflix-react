52afc75bcb15f73cfa1194fc6f77572bee198279
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-13 19:42:34 +0800

    update

--- app/page.js
+++ app/page.js
@@ -4,6 +4,17 @@ import Navbar from '@/components/Navbar'
 import SectionCards from '@/components/SectionCards'
 
 export default function Home() {
+  const disneyVideos = [
+    {
+      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
+    },
+    {
+      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
+    },
+    {
+      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
+    },
+  ];
   return (
     <div className=''>
       {/* Navbar */}
@@ -14,7 +25,8 @@ export default function Home() {
 
       {/* Section Cards */}
       <div className="mt-6">
-        <SectionCards title="Disney" />
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
       </div>
     </div>
   )
--- components/SectionCards.jsx
+++ components/SectionCards.jsx
@@ -1,7 +1,7 @@
 import Card from "./card";
 
 const SectionCards = (props) => {
-    const { title } = props;
+    const { title, videos, size } = props;
 
     return (
         <section className="px-4 text-blue20 bg-black50">
@@ -9,9 +9,9 @@ const SectionCards = (props) => {
                 {title}
             </h2>
             <div className="flex pb-6 mt-6 mr-3 overflow-x-scroll overflow-y-hidden pt-7 ">
-                <Card imgUrl="https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80" size="large" />
-                <Card imgUrl="https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80" size="large" />
-                <Card imgUrl="https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80" size="large" />
+                {videos.map((video, idx) => {
+                    return <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />;
+                })}
             </div>
         </section>
     );
