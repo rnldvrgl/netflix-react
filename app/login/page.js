143ab4d88f55f4d635bf067f5820878622432b06
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-13 21:49:16 +0800

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import NetflixLogo from "@/components/NetflixLogo";
 
 const Login = () => {
     return (
@@ -6,6 +9,16 @@ const Login = () => {
             <Head>
                 <title>Netflix SignIn</title>
             </Head>
+
+            <header>
+                <div className="px-4 flex p-5">
+                    <Link className="flex font-medium text-base items-center text-white10 mb-4" href="/">
+                        <div className="text-red w-32">
+                            <NetflixLogo />
+                        </div>
+                    </Link>
+                </div>
+            </header>
         </div>
     );
 };
