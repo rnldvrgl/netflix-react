a3c9569ae2e3daca92d5652e8f8ba5a2bbd37c93
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-13 21:34:36 +0800

    add popular videos api

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Billboard from '@/components/Billboard'
-import { getVideos } from "@/lib/videos";
+import { getVideos, getPopularVideos } from "@/lib/videos";
 import Navbar from '@/components/Navbar'
 import SectionCards from '@/components/SectionCards'
 
@@ -7,10 +7,10 @@ export default async function Home() {
   const disneyVideos = await getVideos('disney trailer');
   const productivityVideos = await getVideos('productivity');
   const travelVideos = await getVideos('travel');
-  const popularVideos = await getVideos('popular');
+  const popularVideos = await getPopularVideos();
 
   return (
-    <div className=''>
+    <div className='pb-16'>
       {/* Navbar */}
       <Navbar username="delacruz.ronaldvergel@gmail.com" />
 
@@ -18,10 +18,14 @@ export default async function Home() {
       <Billboard title="test" description="testing" imageURL="/images/Kingdom.png" />
 
       {/* Section Cards */}
-      <div className="mt-6">
+      <div className="mt-6 flex flex-col gap-8">
         <SectionCards title="Disney" videos={disneyVideos} size="large" />
         <SectionCards title="Travel" videos={travelVideos} size="small" />
-        <SectionCards title="Productivity" videos={productivityVideos} size="medium" />
+        <SectionCards
+          title="Productivity"
+          videos={productivityVideos}
+          size="medium"
+        />
         <SectionCards title="Popular" videos={popularVideos} size="small" />
       </div>
     </div>
