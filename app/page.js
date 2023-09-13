89e25564a1950402e666031504c58b35c70cd495
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-13 19:45:47 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,11 @@
 import Billboard from '@/components/Billboard'
-import Card from '@/components/Card'
+import { getVideos } from "@/lib/videos";
 import Navbar from '@/components/Navbar'
 import SectionCards from '@/components/SectionCards'
 
 export default function Home() {
-  const disneyVideos = [
-    {
-      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
-    },
-    {
-      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
-    },
-    {
-      imgUrl: "https://images.unsplash.com/photo-1634157703702-3c124b455499?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2128&q=80",
-    },
-  ];
+  const disneyVideos = getVideos();
+
   return (
     <div className=''>
       {/* Navbar */}
