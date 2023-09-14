cff20af35593f71152493b229ca6e46e290f5896
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-14 14:06:09 +0800

    Update page.js

--- app/(dashboard)/(routes)/video/[videoId]/page.js
+++ app/(dashboard)/(routes)/video/[videoId]/page.js
@@ -1,6 +1,9 @@
 "use client"
 
+import Modal from "react-modal";
 import { useParams, useRouter } from 'next/navigation'
+
+// Modal.setAppElement("#__next");
 
 const Video = () => {
     const router = useRouter();
@@ -8,7 +11,17 @@ const Video = () => {
     const videoId = params.videoId;
 
     return (
-        <div>video {videoId} </div>
+        <div>
+            video page {videoId}
+            <Modal
+                isOpen={true}
+                contentLabel="Watch the video"
+                onRequestClose={() => router.back()}
+                overlayClassName={''}
+            >
+                <div>Modal body</div>
+            </Modal>
+        </div>
     );
 }
 
