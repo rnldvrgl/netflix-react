377254f565a05ed57f9cfcbccb5d22339648f64c
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-14 17:58:08 +0800

    Update page.js

--- app/(dashboard)/(routes)/video/[videoId]/page.js
+++ app/(dashboard)/(routes)/video/[videoId]/page.js
@@ -42,7 +42,7 @@ const Video = () => {
                 isOpen={true}
                 contentLabel="Watch the video"
                 onRequestClose={() => router.back()}
-                className="absolute left-0 right-0 my-0 mx-auto w-11/12 bottom-10 bg-black40 top-[10%] outline-none rounded-xl border border-shadow10"
+                className="absolute left-0 right-0 my-0 mx-auto w-11/12 bottom-10 h-fit bg-black40 top-[10%] outline-none rounded-xl border border-white"
                 overlayClassName="top-0 left-0 right-0 bottom-0 w-full h-screen"
             >
                 <iframe
@@ -54,20 +54,20 @@ const Video = () => {
                     className="shadow rounded-t-xl"
                 ></iframe>
 
-                <div className="px-12 py-0">
+                <div className="px-12 py-0 my-5">
                     <div className="grid grid-cols-[minmax(0,2fr),minmax(0,1fr)] gap-x-8">
                         <div className="overflow-y-scroll max-h-96">
-                            <p className="mt-6 mb-2 text-lg text-green10">{publishTime}</p>
+                            <p className="mb-2 text-lg text-green10">{publishTime}</p>
                             <p className="text-lg text-white10">{title}</p>
-                            <p className="mt-3 mb-2">{description}</p>
+                            <p className="mt-3 mb-2 text-sm">{description}</p>
                         </div>
                         <div className="text-white10 text-">
                             <p className="text-[0.875rem] leading-[1.25rem] ml-0 break-words mt-6">
-                                <span className="text-gray10">Cast: </span>
+                                <span className="font-bold text-gray10">Cast: </span>
                                 <span className="m-0 text-white30">{channelTitle}</span>
                             </p>
                             <p className="text-[0.875rem] leading-[1.25rem] ml-0 break-words mt-6">
-                                <span className="text-gray10">View Count: </span>
+                                <span className="font-bold text-gray10">View Count: </span>
                                 <span className="m-0 text-white30">{viewCount}</span>
                             </p>
                         </div>
