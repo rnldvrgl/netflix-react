609a308765089b9416566262bc061dff2b927f1c
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-14 14:12:13 +0800

    Update page.js

--- app/(dashboard)/(routes)/video/[videoId]/page.js
+++ app/(dashboard)/(routes)/video/[videoId]/page.js
@@ -12,12 +12,13 @@ const Video = () => {
 
     return (
         <div>
-            video page {videoId}
+            {/* video page {videoId} */}
             <Modal
                 isOpen={true}
                 contentLabel="Watch the video"
                 onRequestClose={() => router.back()}
-                overlayClassName={''}
+                className="absolute left-0 right-0 mx-0 my-auto w-full bottom-10 bg-black40 top-[10%] outline-none rounded-xl border border-shadow10"
+                overlayClassName="top-0 left-0 right-0 botton-0 w-full h-screen"
             >
                 <div>Modal body</div>
             </Modal>
