a1c82931ea7ed133c2a0714820bdbf5c2ff2dd02
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-15 13:13:09 +0800

    Update route.js

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
-
+import { magicAdmin } from "@/lib/magic"
 export async function POST(request) {
     try {
         const auth = request.headers.authorization;
         const didToken = auth ? auth.substr(7) : "";
-        //invoke magic
+
+        const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+
         return NextResponse.json({ didToken, status: 200, done: true });
     } catch (error) {
         console.error("Something went wrong logging in", error);
