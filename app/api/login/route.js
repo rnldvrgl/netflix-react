a8ffb9104668fbce8179fece508acb1607993467
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-15 13:08:55 +0800

    update

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,17 +1,11 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
-    try {
-        return NextResponse.json({ status: 200, done: true });
-    } catch (error) {
-        console.error("Something went wrong logging in", error);
-        return NextResponse.json({ status: 500, done: false });
-    }
-}
-
 export async function POST(request) {
     try {
-        return NextResponse.json({ status: 200, done: true });
+        const auth = request.headers.authorization;
+        const didToken = auth ? auth.substr(7) : "";
+        //invoke magic
+        return NextResponse.json({ didToken, status: 200, done: true });
     } catch (error) {
         console.error("Something went wrong logging in", error);
         return NextResponse.json({ status: 500, done: false });
