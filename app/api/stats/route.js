e6ef7f6c8ea4c5e0d29fa81a16eda2a1ea7f962f
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-19 10:44:10 +0800

    Update route.js

--- app/api/stats/route.js
+++ app/api/stats/route.js
@@ -5,9 +5,20 @@ export async function POST(request) {
     const cookieStore = cookies();
     const tokenCookie = cookieStore.get('token');
 
-    if (!tokenCookie) {
-        return NextResponse.json({ message: "Unauthorized" }, { status: 403 })
-    } else {
-        return NextResponse.json({ message: "Authorized" }, { status: 200 });
+    try {
+        let message = !tokenCookie ? "Unauthorized" : "Authorized";
+        let status = !tokenCookie ? 403 : 200;
+
+        // if (!tokenCookie) {
+        //     return NextResponse.json({ message: "Unauthorized" }, { status: 403 })
+        // } else {
+        //     return NextResponse.json({ message: "Authorized" }, { status: 200 });
+        // }
+
+        return NextResponse.json({ message }, { status });
+    } catch (error) {
+        console.log("Error occured while checking token: ", error?.message);
+
+        return NextResponse.json({ done: false, error: error?.message }, { status: 500 });
     }
 }
