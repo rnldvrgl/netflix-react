19d945ad4854206ba5c9efdded7efb5edffd5afa
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-19 10:36:07 +0800

    Update route.js

--- app/api/stats/route.js
+++ app/api/stats/route.js
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import { cookies } from 'next/headers'
 
 export async function POST(request) {
-    return NextResponse.json({ status: 200, message: "Hello World" });
+    const cookieStore = cookies();
+    const tokenCookie = cookieStore.get('token');
+
+    if (!tokenCookie) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 403 })
+    } else {
+        return NextResponse.json({ message: "Authorized" }, { status: 200 });
+    }
 }
