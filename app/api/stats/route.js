1ad9630e980180bfd108c7a70fbe6871d5afa3fd
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-23 20:57:55 +0800

    update hasura js

--- app/api/stats/route.js
+++ app/api/stats/route.js
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
-import { findVideoIdByUser } from "@/lib/db/hasura";
+import { findVideoIdByUser, updateStats } from "@/lib/db/hasura";
 
 export async function POST(request) {
     try {
@@ -16,12 +16,22 @@ export async function POST(request) {
         const videoId = request.nextUrl.searchParams.get('videoId');
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         const userId = decodedToken.issuer;
-        const findVideoId = await findVideoIdByUser(token, userId, videoId);
-
+        const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
         const message = "Authorized";
-        const status = 200;
 
-        return NextResponse.json({ findVideoId, message }, { status });
+        if (doesStatsExist) {
+            const status = 200;
+            const response = await updateStats(token, {
+                watched: false,
+                favourited: 2,
+                userId,
+                videoId,
+            });
+            return NextResponse.json({ response, message }, { status });
+        } else {
+            const status = 500;
+            return NextResponse.json({ doesStatsExist, message }, { status });
+        }
     } catch (error) {
         console.error("Error occurred while checking token:", error?.message);
 
