5888be14b800dd30c74066004126ca496df0917d
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-26 07:36:42 +0800

    Update getIsAuthenticated.js

--- actions/getIsAuthenticated.js
+++ actions/getIsAuthenticated.js
@@ -6,10 +6,13 @@ export const getIsAuthenticated = () => {
         try {
             const token = cookies().get("token")?.value;
             const userId = await verifyToken(token);
-            resolve({
-                userId,
-                token
-            });
+
+            setTimeout(() => {
+                resolve({
+                    userId,
+                    token
+                })
+            }, 1000)
         } catch (error) {
             console.error("Error checking user authentication:", error);
             resolve({
