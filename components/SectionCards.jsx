9ff101269954b6fab9fd775e1b75edf0ec948b29
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-09-25 22:47:59 +0800

    Update SectionCards.jsx

--- components/SectionCards.jsx
+++ components/SectionCards.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Card from "./card";
+import Card from "@/components/Card";
 
 const SectionCards = (props) => {
     const { title, videos = [], size, shouldWrap = false, shouldScale, justify = "start" } = props;
