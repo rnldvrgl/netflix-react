1f95eb6e669e6b2da152fd85a28b755525feccf8
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-25 15:22:43 +0800

    Update page.js

--- app/(root)/(dashboard)/browse/(routes)/my-list/page.js
+++ app/(root)/(dashboard)/browse/(routes)/my-list/page.js
@@ -1,5 +1,23 @@
+import Navbar from "@/components/Navbar";
+import SectionCards from "@/components/SectionCards";
+import Head from "next/head";
+
 export default async function MyList() {
     return (
-        <div>My List</div>
+        <>
+            <Head>
+                <title>My List</title>
+            </Head>
+
+            <main className="pb-12 mt-24 ">
+                {/* Navbar */}
+                < Navbar />
+
+                {/* Section Cards */}
+                <div className="flex flex-col gap-y-12" >
+                    <SectionCards title="My List" videos={[]} size="small" />
+                </div>
+            </main>
+        </>
     )
 }
