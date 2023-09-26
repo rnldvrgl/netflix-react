a02eedb4b87b502ae282860a3847c9e804d50c88
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-26 09:34:50 +0800

    Update getIsAuthenticated.js

--- actions/getIsAuthenticated.js
+++ actions/getIsAuthenticated.js
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { verifyToken } from "@/lib/utils";
 
 export const getIsAuthenticated = () => {
+    'use server'
     return new Promise(async (resolve) => {
         const token = cookies().get("token")?.value;
         const userId = await verifyToken(token);
