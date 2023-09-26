dca2e5297d1e460f42bda3d206a9fecf359a197d
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-09-26 09:36:04 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -7,17 +7,22 @@ export const config = {
 }
 
 export async function middleware(request) {
-    const token = request.cookies.get("token")?.value || '';
-    const userId = await verifyToken(token);
-    const { pathname } = request.nextUrl;
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        const userId = await verifyToken(token);
+        const { pathname } = request.nextUrl;
 
-    const isPublicPath = pathname === '/sign-in' || pathname === '/';
+        const isPublicPath = pathname === '/sign-in' || pathname === '/';
 
-    if (isPublicPath && token) {
-        return NextResponse.redirect(new URL('/browse', request.nextUrl));
-    }
+        if (isPublicPath && token) {
+            return NextResponse.redirect(new URL('/browse', request.nextUrl));
+        }
 
-    if (!token || !userId) {
-        return NextResponse.redirect(new URL('/sign-in', request.nextUrl));
+        if (!token || !userId) {
+            return NextResponse.redirect(new URL('/sign-in', request.nextUrl));
+        }
+    } catch (error) {
+        console.error('Middleware error:', error);
+        return NextResponse.error();
     }
 }
